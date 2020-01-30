562262109d9d034c658410fd0e73f53b95fb1b89
DiaZa13 <54074539+DiaZa13@users.noreply.github.com>
2020-01-30 11:20:55 -0600

    Controlador

---
+++ cStack.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * @author Diana Zaray Corado (191025) && Javier Alejandro Cotto (19324)
+ * @version 30/01/2020
+ * Controlador de Stack
+ *
+ */
+public class cStack  implements Stack{
+
+	@Override
+	public void push(Object item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
