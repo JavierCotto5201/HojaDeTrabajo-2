4f09192fcb164d168d8172a8807aea1f0e015906
DiaZa13 <54074539+DiaZa13@users.noreply.github.com>
2020-02-04 17:41:53 -0600

    Nueva interfaz calculadora y su implementacion

---
+++ Calculadora.java
@@ -0,0 +1,36 @@
+
+public class Calculadora implements iCalculadora {
+
+	@Override
+	public int sumar(int operando1, int operando2) {
+		// TODO Auto-generated method stub
+		int suma = 0;
+		suma = operando1 + operando2;
+		return suma;
+	}
+
+	@Override
+	public int restar(int operando1, int operando2) {
+		// TODO Auto-generated method stub
+		int resta = 0;
+		resta = operando1 + operando2;
+		return resta;
+	}
+
+	@Override
+	public int multiplicar(int operando1, int operando2) {
+		// TODO Auto-generated method stub
+		int multiplicar = 0;
+		multiplicar = operando1 * operando2;
+		return multiplicar;
+	}
+
+	@Override
+	public int dividir(int operando1, int operando2) {
+		// TODO Auto-generated method stub
+		int dividir = 0;
+		dividir = (int)(operando1/operando2);
+		return dividir;
+	}
+
+}
---
+++ iCalculadora.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author Diana Zaray Corado #191025 & Javier Alejandro Cotto #19324
+ * @version 4/02/20
+ *
+ */
+public interface iCalculadora {
+	
+	//Ingresan dos valores numericos y realiza una suma
+	public int sumar (int operando1, int operando2);
+	
+	//Ingresan dos valores numericos y realiza una resta
+	public int restar (int operando1, int operando2);
+	
+	//Ingresan dos valores numericos y realiza una multiplicacion
+	public int multiplicar (int operando1, int operando2);
+	
+	//Ingresan dos valores numericos y realiza una division
+	public int dividir(int operando1, int operando2);
+
+}
