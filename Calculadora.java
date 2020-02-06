eaba5cfb68fb98f493bd333274b1be6b24e1eb00
DiaZa13 <54074539+DiaZa13@users.noreply.github.com>
2020-02-05 23:45:11 -0600

    Manejo de errores (division entre 0)

--- Calculadora.java
+++ Calculadora.java
@@ -7,6 +7,12 @@
 public class Calculadora implements iCalculadora {
 
 	@Override
+	/**
+	 * Sumar
+	 * @param int operando1, int operando2
+	 * Realiza la suma entre dos valores
+	 * @return int suma
+	 */
 	public int sumar(int operando1, int operando2) {
 		// TODO Auto-generated method stub
 		int suma = 0;
@@ -15,6 +21,12 @@ public class Calculadora implements iCalculadora {
 	}
 
 	@Override
+	/**
+	 * Restar
+	 * @param int operando1, int operando2
+	 * Realiza la resta entre dos valores
+	 * @return int resta
+	 */
 	public int restar(int operando1, int operando2) {
 		// TODO Auto-generated method stub
 		int resta = 0;
@@ -23,6 +35,12 @@ public class Calculadora implements iCalculadora {
 	}
 
 	@Override
+	/**
+	 * Multiplicar
+	 * @param int operando1, int operando2
+	 * Realiza la multiplicacion entre dos valores
+	 * @return int multiplicar
+	 */
 	public int multiplicar(int operando1, int operando2) {
 		// TODO Auto-generated method stub
 		int multiplicar = 0;
@@ -31,10 +49,21 @@ public class Calculadora implements iCalculadora {
 	}
 
 	@Override
+	/**
+	 * Dividir 
+	 * @param int operando1, int operando2
+	 * Realiza la division entre dos valores
+	 * @return dividir
+	 */
 	public int dividir(int operando1, int operando2) {
 		// TODO Auto-generated method stub
 		int dividir = 0;
-		dividir = (int)(operando1/operando2);
+		try{
+			dividir = (int)(operando1/operando2);
+		}catch(Exception e) {
+			dividir = -1;
+		}
+		
 		return dividir;
 	}
 
